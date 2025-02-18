f5ded12ed5d0062372087d1b533a03e185ec70ba
ard303 <ayansajjad1010@gmail.com>
2025-02-18 19:09:15 +0500

    final

--- arm.py
+++ arm.py
@@ -1,12 +1,12 @@
 print("if you want to write a three digit number and check if it is")
-num1=int(input("enter the first digit with two zeros"))
-num2=int(input("enter the second digit with one zero"))
+num1=int(input("enter the first digit"))
+num2=int(input("enter the second digit"))
 num3=int(input("enter the third digit"))
-ognum=num1+num2+num3
-num4=num1/100
-num5=num2/10
+num4=num1*100
+num5=num2*10
+ognum=num4+num5+num3
 print("your number is ",ognum)
-if(num4**3+num5**3+num3**3==ognum):
+if(num1**3+num2**3+num3**3==ognum):
     print("the number is a armstrong number")
 else:
     print("the number is not a armstrong")
