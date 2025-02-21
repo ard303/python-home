b4a4f1e6ccf74c87ecf597a72e144691673c004b
ard303 <ayansajjad1010@gmail.com>
2025-02-21 20:13:37 +0500

    final

---
+++ hexagon.py
@@ -0,0 +1,22 @@
+import turtle
+screen=turtle.Screen()
+screen.setup(600,600)
+screen.bgcolor("blue")
+board=turtle.Turtle()
+
+board.forward(50)
+board.left(60)
+board.forward(50)
+board.left(60)
+board.forward(50)
+board.left(60)
+board.forward(50)
+board.left(60)
+board.forward(50)
+board.left(60)
+board.forward(50)
+board.left(60)
+
+
+
+turtle.done()
---
+++ triangle.py
@@ -0,0 +1,12 @@
+import turtle
+screen=turtle.Screen()
+screen.setup(400,400)
+screen.bgcolor("blue")
+board=turtle.Turtle()
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+turtle.done()
